f4675644a6260781f339c257563d924baad4b649
Hanganu Vlad <160888433+hanganuvlad@users.noreply.github.com>
2024-05-22 00:38:42 +0300

    Update Licenta.ino
    Updating old code.

--- Licenta.ino
+++ Licenta.ino
@@ -1,192 +1,91 @@
-#define viteza_mica 50
-#define viteza_micaB 70
+#include <Servo.h>
+
+#define viteza_mica 103
+#define viteza_micaB 150
 #define viteza_mare 255
-#define viteza_mareB 100
+#define viteza_mareB 150
+
+int servoPin = 5;
+Servo Servo1; 
+
+float c;
 
 //motor MARE
-int dirmotorA = 8;
-int pwmmotorA = 9;
+int dirmotorA=2;
+int pwmmotorA=3;
 
-//motor mic
-int dir1motorB = 10;
-int dir2motorB = 11;
-int analogpin = 2;
-int sem = 2;
+//Char control
+char receivedMessage[7];
+char *ptr = 0;
+double steering = 0;
+double m_speed = 0;
+int test,val;
 
 void slowdown(int viteza)
 {
-  if (sem == 2)
-    analogWrite(pwmmotorA, 0);
-  else
-  {
     for (int i=viteza; i>=80; i--)
     {
-      analogWrite(pwmmotorA, i);
+      analogWrite(pwmmotorA,i);
       delay(10);
     }
-    analogWrite(pwmmotorA, 0);
-  }
+    analogWrite(pwmmotorA,0);
 }
 
 void setup() 
 {
-  pinMode(dirmotorA, OUTPUT);
-  pinMode(pwmmotorA, OUTPUT);
+  pinMode(dirmotorA,OUTPUT);
+  pinMode(pwmmotorA,OUTPUT);
+  Servo1.attach(servoPin); 
+  Servo1.write(63);
   
-  pinMode(dir1motorB, OUTPUT);
-  pinMode(dir2motorB, OUTPUT);
-  pinMode(analogpin, OUTPUT);
+  pinMode(13,OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() 
-{
-  int i,viteza,val;
-  boolean isValidInput;
-  Serial.println( "-----------------------------" );
-  Serial.println( "MENU:" );
-  Serial.println( "1) Fast SPEED" );
-  Serial.println( "2) Low SPEED" );
-  Serial.println( "3) Soft stop (coast)" );
-  Serial.println( "4) Left" );
-  Serial.println( "5) Right" );
-  Serial.println( "6) Hard stop (brake)" );
-  Serial.println( "7) Forward" );
-  Serial.println( "-----------------------------" );
-  do
+{ 
+  if (Serial.available())
   {
-    byte c;
-    while( !Serial.available() );
-    c = Serial.read();
-    switch( c )
+    
+    memset(receivedMessage, '\0', sizeof(receivedMessage));
+    Serial.readBytesUntil('\r', receivedMessage, 5);
+    
+    ptr = &(receivedMessage[0]);
+    if(*ptr == 'S')
     {
-      case '1': // 1) Fast SPEED
-        Serial.println( "Fast SPEED..." );
-        digitalWrite(dirmotorA, LOW);
-        analogWrite(pwmmotorA, viteza_mare);
-        sem = 1;
-        isValidInput = true;
-        break;      
-         
-      case '2': // 2) Low SPEED      
-        Serial.println( "Low SPEED..." );
-        digitalWrite(dirmotorA, LOW);
-        analogWrite(pwmmotorA, viteza_mica);
-        sem = 0;
-        isValidInput = true;
-        break;
-         
-      case '3': // 3) Soft stop
-        Serial.println( "Soft stop" );
-        digitalWrite(dirmotorA, LOW);
-        if (sem == 1)
-          viteza = viteza_mare;
-        else
-          viteza = viteza_mica;
-          
-        slowdown(viteza);
+      ptr++;
 
-        digitalWrite(dir1motorB, LOW);
-        digitalWrite(dir2motorB, LOW);
-        isValidInput = true; sem = 2;
-        break;      
- 
-      case '4': // 4) Left
-        Serial.println( "Left" );
-        digitalWrite(dirmotorA, LOW);
-        if (sem == 1)
-          analogWrite(pwmmotorA, viteza_mare);
-        else
-          analogWrite(pwmmotorA, viteza_mica);
-        
-        digitalWrite(dir1motorB, LOW);
-        digitalWrite(dir2motorB, HIGH);
-        analogWrite(dir1motorB, viteza_mareB);
-        //digitalWrite(dir2motorB, LOW);
-
-        delay(1000);
-        val = analogRead(analogpin);
-        Serial.println(val);
-        isValidInput = true;
-        break;      
-      
-      case '5': // 5) Right
-        Serial.println( "Right" );
-        digitalWrite(dirmotorA,LOW);
-        if (sem == 1)
-          analogWrite(pwmmotorA, viteza_mare);
-        else
-          analogWrite(pwmmotorA, viteza_mica);
-        
-        digitalWrite(dir1motorB, HIGH);
-        digitalWrite(dir2motorB, LOW);
-        analogWrite(dir2motorB, viteza_mareB);
-        //digitalWrite(dir1motorB, LOW);
-        delay(1000);
-        
-        isValidInput = true;
-        val = analogRead(analogpin);
-        Serial.println(val);
-        break;
-       
-      case '6': // 6) Hard stop
-        Serial.println( "Hard stop" );
-        if (sem == 1)
-          analogWrite(pwmmotorA, viteza_mare);
-        else
-          analogWrite(pwmmotorA, viteza_mica);
-        analogWrite(pwmmotorA,0);
-        
-        digitalWrite(dir1motorB, LOW);
-        digitalWrite(dir2motorB, LOW);
-        isValidInput = true;
-        break;      
-      
-      case '7': // 7) Forward
-        
-        Serial.println(sem);
-        Serial.println( "Forward" );
-        digitalWrite(dirmotorA, LOW);
-        
-        if (sem == 1)
-          analogWrite(pwmmotorA, viteza_mare);
-        else
-          analogWrite(pwmmotorA, viteza_mica);
-        
-        val = analogRead(analogpin);
-        if (val <= 450)
-        {
-          digitalWrite(dir1motorB, LOW);
-          digitalWrite(dir2motorB, HIGH);
-          while (val <= 400)
-          {
-            analogWrite(dir2motorB, 150);
-            val = analogRead(analogpin);
-            Serial.println(val);
-          }
-          digitalWrite(dir2motorB, LOW);
-        }
-        else
-        {
-          digitalWrite(dir1motorB, HIGH);
-          digitalWrite(dir2motorB, LOW);
-          while (val >= 600)
-          {
-            analogWrite(dir1motorB, 150);
-            val = analogRead(analogpin);
-            Serial.println(val);
-          }
-          digitalWrite(dir1motorB, LOW);
-        }
-        
-        isValidInput = true;
-        break;
-      default:
-        if (c > 7 || c < 1)
-          Serial.println("Wrong character!");
-        
-        isValidInput = false;
-        break;
+      steering = 0;
+      for(int i = strlen(ptr)-1; *ptr != '\0'; i--)
+      {
+        steering += (*ptr++ - '0') * pow(10,i);
+      }
+      steering = round(steering);
+      Servo1.write(steering);
+      delay(10); 
     }
-  } while( isValidInput == true );
+    else if(*ptr == 'M')
+    {
+      ptr++;
+
+      m_speed = 0;
+      for(int i = strlen(ptr)-1; *ptr != '\0'; i--)
+      {
+        m_speed += (*ptr++ - '0') * pow(10,i);  
+      }
+      m_speed = round(m_speed);
+      if(m_speed == 200)
+      {
+        digitalWrite(13,HIGH); 
+        digitalWrite(dirmotorA,LOW);
+        analogWrite(pwmmotorA,viteza_mica);
+      }
+      else
+      {
+        digitalWrite(13,LOW); 
+        slowdown(viteza_mica);
+      }
+    } 
+  }
+  
 }
